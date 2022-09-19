78ad817ccdb2b06307e3bd6f20c0be022a910adf
Azeem Mohammad <mdazeem5790@gmail.com>
2022-09-19 20:01:48 +0530

    HackerRank Solutions Java

---
+++ src/programs/BitPuzzle.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class BitPuzzle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mask = 0x000F;
+		int value = 0x2222;
+		System.out.println(mask & value);
+
+	}
+
+}
