b0278aee5daac1dfbc88969ec09f251951f6e150
Azeem Mohammad <mdazeem5790@gmail.com>
2022-09-21 10:46:50 +0530

    Alice Beautiful Binary String - HackerRank

---
+++ src/programs/StrBeautifulBinary.java
@@ -0,0 +1,22 @@
+package programs;
+
+public class StrBeautifulBinary {
+
+	public static void main(String[] args) {
+
+		String input = "0101010";
+		int n = input.length();
+		int answer = 0;
+		int i = 0;
+		while (i < n - 2) {
+			if (input.substring(i, i + 3).equals("010")) {
+				answer++;
+				i += 3;
+				continue;
+			}
+			i++;
+		}
+		System.out.println(answer);
+
+	}
+}
