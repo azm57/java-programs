5c0d1af78d23e663ab94e80bd2e7a40a62c50ad9
Azeem Mohammad <mdazeem5790@gmail.com>
2022-09-21 12:57:41 +0530

    Modify DrawingBook

--- src/programs/DrawingBookPageTurns.java
+++ src/programs/DrawingBookPageTurns.java
@@ -5,9 +5,9 @@ public class DrawingBookPageTurns {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n = 6;
-		int p = 3;
+		int p = 2;
 		int frontD = p / 2;
-		int backD = n % 2 == 0 ? (n - p + 1 / 2) + 1 : n - p / 2;
+		int backD = n % 2 == 0 ? (n - p + 1 / 2) : n - p / 2;
 		System.out.println(Math.min(frontD, backD));
 
 	}
